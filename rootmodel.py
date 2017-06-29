86458263e80f55977778519f2bd5520e2709649c
ll698 <ll698@cornell.edu>
2017-06-28 21:49:33 -0400

    test

--- rootmodel.py
+++ rootmodel.py
@@ -8,25 +8,28 @@ from keras.layers import Conv2D, MaxPooling2D
 
 # Untrained architecture for root node
 
-model = Sequential()
-model.add(Conv2D(32, (3, 3), padding='same', input_shape=(3, 32, 32)))
-model.add(Activation('relu'))
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
 
-model.add(Conv2D(64, (3, 3), padding='same'))
+def newModel():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), padding='same', input_shape=(3, 32, 32)))
+    model.add(Activation('relu'))
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
 
-model.add(Activation('relu'))
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+    model.add(Conv2D(64, (3, 3), padding='same'))
 
-model.add(Flatten())
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(10))
-model.add(Activation('softmax'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(10))
+    model.add(Activation('softmax'))
+    return model

5bbe6832f4174f31316c3979c2b599303a80d5c3
ll698 <ll698@cornell.edu>
2017-06-28 20:24:12 -0400

    temp removed preproc

--- run_network.py
+++ run_network.py
@@ -14,7 +14,7 @@ root_network = networkhandler.Network("root", 32, 10, 200, True, dataset)
 #define root network model
 #root_network.define_model(rootmodel.model)
 root_network.load_model("models/root.h5")
-root_network.preprocess()
+#root_network.preprocess()
 root_network.optimizer(0.0001, 1e-6)
 root_network.compile()
 root_network.save_model()
